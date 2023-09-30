099d8f5830734ece7376c07785cd5d6bfa2cca1a
wangbairu <1240738260@qq.com>
2023-09-30 11:19:47 +0800

    A Basic Starting Point

---
+++ Step1/tutorial.cxx
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+#include "TutorialConfig.h"
+
+int main()
+{
+
+    cout<<"first cmake program!!!"<<endl;
+    cout<<Tutorial_VERSION_MAJOR<<"."<<Tutorial_VERSION_MINOR<<endl;
+    return 0;
+}
